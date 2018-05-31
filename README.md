021a376757521860d80d212d44ddce74c8eb2507
tomjamescn <tomjamescn@qq.com>
2018-05-31 09:46:20 +0800

    more features

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # gotmpl
 Template generation tools stands for GOlang TeMPLate.
 
-Maybe [Mustache for Golang](https://github.com/cbroglie/mustache) is already a good solution for template generation, BUT, it is not perfect because it has not folder name including tempalte palceholder, more binding data input format, comment for binding data and etc.
+Maybe [Mustache for Golang](https://github.com/cbroglie/mustache) is already a good solution for single template generation, BUT, it is not perfect because it does not support folder name including placeholder, more binding data input format, comment for binding data and etc.
 
 # feature
-- using [Mustache](https://mustache.github.io/) sytanx for template file
-- directory name can be used as template
-- json file, yaml file and input arguments as binding data
-- comment for binding data
+- [x] using [Mustache](https://mustache.github.io/) sytanx for template file
+- [x] support placeholder in folder name
+- [x] json file, yaml file and input arguments as binding data
+- [] summary for all placeholder
+- [] comment support for placeholder
+- [] git repository as template path
 - may be more...
 
 # install
@@ -15,14 +17,18 @@ Maybe [Mustache for Golang](https://github.com/cbroglie/mustache) is already a g
 go get github.com/tomjamescn/gotmpl
 ```
 
-# use
+# usage
 ```sh
 #using test_data including in gotmpl src directory
 cd $GOPATH/github/tomjamescn/gotmpl
-gotmpl -bindingDataPath `pwd`/test_data/test.json -templatePath `pwd`/test_data -outputPath /tmp/gotmpl/output/test
+gotmpl -b `pwd`/test_data/test.json -t `pwd`/test_data -o /tmp/gotmpl/output/test
 
 #using input arguments as binding data
-gotmpl -templatePath `pwd`/test_data -outputPath /tmp/gotmpl/output/test index=index
+gotmpl -t `pwd`/test_data -o /tmp/gotmpl/output/test index=index
+
+#show summary for all placeholder
+gotmpl -t `pwd`/test_data -s 
+
 ```
 
 # thanks
--- main.go
+++ main.go
@@ -2,6 +2,8 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"flag"
@@ -10,10 +12,14 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"regexp"
 	"runtime"
 	"strings"
 
 	"github.com/cbroglie/mustache"
+	"golang.org/x/crypto/ssh"
+	"gopkg.in/src-d/go-git.v4"
+	gitssh "gopkg.in/src-d/go-git.v4/plumbing/transport/ssh"
 	yaml "gopkg.in/yaml.v2"
 )
 
@@ -30,7 +36,7 @@ func listTemplateFilePath(templatePath string) ([]string, error) {
 			panic(err)
 		}
 		for _, f := range fileInfoes {
-			filePaths, err := listTemplateFilePath(templatePath + string(filepath.Separator) + f.Name())
+			filePaths, err := listTemplateFilePath(path.Join(templatePath, f.Name()))
 			if err != nil {
 				return nil, err
 			}
@@ -92,19 +98,184 @@ func parseBindingData(bindingDataPath string) (interface{}, error) {
 	return data, nil
 }
 
+func StringStartWith(str string, subStrs []string, caseSensitive bool) (string, bool) {
+	str = strings.Trim(str, " \n\t")
+	if !caseSensitive {
+		str = strings.ToLower(str)
+	}
+	for _, sub := range subStrs {
+		prefix := sub
+		if !caseSensitive {
+			prefix = strings.ToLower(prefix)
+		}
+		if strings.HasPrefix(str, prefix) {
+			return sub, true
+		}
+	}
+
+	return "", false
+}
+
+func NormalizePath(path string) string {
+	r := regexp.MustCompile("/{2,}")
+	return string(r.ReplaceAll([]byte(path), []byte("")))
+}
+
+func GetMD5Hash(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func getRealTemplatePath(templatePath, subTemplatePath string) (p string, tmpPath string, isRepository bool) {
+	if _, ok := StringStartWith(templatePath, []string{"http://", "https://", "file://", "git://", "ssh://"}, true); ok {
+		//repository
+
+		tmpDir := GetMD5Hash(templatePath)
+		tmpPath = path.Join(os.TempDir(), string(tmpDir))
+		cloneOptions := &git.CloneOptions{
+			URL:      templatePath,
+			Progress: os.Stdout,
+		}
+		fmt.Println(tmpPath)
+		err := os.RemoveAll(tmpPath)
+		if err != nil {
+			panic(err)
+		}
+		_, err = git.PlainClone(tmpPath, false, cloneOptions)
+		if err != nil {
+			err = os.RemoveAll(tmpPath)
+			if err != nil {
+				panic(err)
+			}
+			s := fmt.Sprintf("%s/.ssh/id_rsa", os.Getenv("HOME"))
+			sshKey, err := ioutil.ReadFile(s)
+			signer, err := ssh.ParsePrivateKey([]byte(sshKey))
+			auth := &gitssh.PublicKeys{User: "git", Signer: signer}
+			cloneOptions.Auth = auth
+			_, err = git.PlainClone(tmpPath, false, cloneOptions)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		return path.Join(tmpPath, subTemplatePath), tmpPath, true
+	} else {
+		return NormalizePath(path.Join(templatePath, subTemplatePath)), "", false
+	}
+}
+
+type summary struct {
+	DirPathList  []string
+	FilePathList []string
+}
+
+func showPlaceholderSummary(templatePath string) {
+	tmpFilePaths, err := listTemplateFilePath(templatePath)
+	if err != nil {
+		panic(err)
+	}
+
+	allPlaceholders := make(map[string]bool, 0)
+	placeholders := make(map[string]*summary)
+	comments := make(map[string]string)
+	r4Placeholder := regexp.MustCompile(`\{\{\s*([^\}\s]*)\s*\}\}`)
+	r4Comment := regexp.MustCompile(`\{\{\!\s*([^\}:]*)\s*:\s*([^\}]*)\s*\}\}`)
+	for _, p := range tmpFilePaths {
+		content, err := ioutil.ReadFile(p)
+		if err != nil {
+			panic(err)
+		}
+		retD := r4Placeholder.FindAllStringSubmatch(p, -1)
+		retP := r4Placeholder.FindAllStringSubmatch(string(content), -1)
+		retC := r4Comment.FindAllStringSubmatch(string(content), -1)
+
+		for _, v := range retD {
+			allPlaceholders[v[1]] = true
+			s, ok := placeholders[v[1]]
+			if !ok {
+				s = &summary{}
+				s.DirPathList = make([]string, 0)
+				s.FilePathList = make([]string, 0)
+				placeholders[v[1]] = s
+			}
+			s.DirPathList = append(s.DirPathList, p)
+		}
+
+		for _, v := range retP {
+			allPlaceholders[v[1]] = true
+			s, ok := placeholders[v[1]]
+			if !ok {
+				s = &summary{}
+				s.DirPathList = make([]string, 0)
+				s.FilePathList = make([]string, 0)
+				placeholders[v[1]] = s
+			}
+			s.FilePathList = append(s.FilePathList, p)
+		}
+
+		for _, v := range retC {
+			comments[v[1]] = v[2]
+		}
+
+	}
+
+	for p, _ := range allPlaceholders {
+		fmt.Println("placeholder:", p)
+		if v, ok := comments[p]; ok {
+			fmt.Println("comment:", v)
+		} else {
+			fmt.Println("comment:")
+		}
+
+		fmt.Println("dir path list:")
+
+		if v, ok := placeholders[p]; ok {
+			for _, l := range v.DirPathList {
+				fmt.Println(l)
+			}
+		}
+
+		fmt.Println("file list:")
+
+		if v, ok := placeholders[p]; ok {
+			for _, l := range v.FilePathList {
+				fmt.Println(l)
+			}
+		}
+
+		fmt.Println("")
+	}
+}
+
 func main() {
 	var templatePath string
+	var subTemplatePath string
 	var outputPath string
 	var bindingDataPath string
+	var summaryFlag bool
 	var bindingDatasFromArgs map[string]string
 
 	bindingDatasFromArgs = make(map[string]string)
 	_, currentFilePath, _, _ := runtime.Caller(0)
 	curDirPath := path.Dir(currentFilePath)
-	flag.StringVar(&templatePath, "templatePath", "", "template path, can be a folder or a file path")
-	flag.StringVar(&outputPath, "outputPath", curDirPath, "output path")
-	flag.StringVar(&bindingDataPath, "bindingDataPath", "", "data for variables in template, can be a yaml or json file path")
+	flag.StringVar(&templatePath, "t", "", "template path, can be a folder, a file path or a git repository")
+	flag.StringVar(&subTemplatePath, "s", "", "sub template path append to template path, can be a folder, a file path or a git repository")
+	flag.StringVar(&outputPath, "o", curDirPath, "output path")
+	flag.StringVar(&bindingDataPath, "b", "", "binding data file path, can be a yaml or json file path")
+	flag.BoolVar(&summaryFlag, "p", false, "show placeholder summary")
 	flag.Parse()
+
+	if templatePath == "" {
+		panic("-t can not be empty")
+	}
+
+	templatePath, tmpPath, isRepository := getRealTemplatePath(templatePath, subTemplatePath)
+
+	if summaryFlag {
+		showPlaceholderSummary(templatePath)
+		return
+	}
 
 	context := make([]interface{}, 0)
 	if bindingDataPath != "" {
@@ -133,6 +304,14 @@ func main() {
 	}
 	for _, p := range templateFilePaths {
 		if err = renderAndSave(p, templatePath, outputPath, context...); err != nil {
+			panic(err)
+		}
+	}
+
+	if isRepository {
+		fmt.Println(tmpPath)
+		err = os.RemoveAll(tmpPath)
+		if err != nil {
 			panic(err)
 		}
 	}
